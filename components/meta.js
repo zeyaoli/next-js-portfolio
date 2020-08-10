26d3adeae057482c087ca572d76eeafab783747a
zeyaoli <zeyao.li.315@gmail.com>
2020-08-10 15:30:24 -0400

    Replace favicon and Add More Metadata

--- components/meta.js
+++ components/meta.js
@@ -1,15 +1,22 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-const name = 'Zeyao Li';
+const name = "Zeyao Li";
 
-export const siteTitle = 'Zeyao Li Portfolio';
+export const siteTitle = "Zeyao Li Portfolio";
+export const pageDescription = "Zeyao Li's portfolio built with Next.js";
 
 export default function Meta() {
-	return (
-		<Head>
-			<link rel="icon" href="/favicon.ico" />
-			<meta name="description" content="Zeyao Li's portfolio built with Next.js" />
-			<meta name="og:title" content={siteTitle} />
-		</Head>
-	);
+  return (
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content={pageDescription} />
+      {/* Open Graph */}
+      <meta name="og:title" content={siteTitle} />
+      <meta property="og:url" content="https://zeyaoli.com/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:description" content={pageDescription} />
+    </Head>
+  );
 }
